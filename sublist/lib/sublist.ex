9eb5afeca18f414cd66edcb0d7aea92e0ba47d47
isabelceer <izabela.cieslewicz@gmail.com>
2023-12-18 12:46:42 +0100

    next exercises done

--- sublist/lib/sublist.ex
+++ sublist/lib/sublist.ex
@@ -4,17 +4,35 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) when is_list(a) and is_list(b) do
-    cond do
-      a == b -> :equal
-      length(a) < length(b) -> if(is_sublist?(a, b), do: :sublist, else: :unequal)
-      length(a) > length(b) -> if(is_sublist?(b, a), do: :superlist, else: :unequal)
-      true -> :unequal
+    case {contains?(a, b), contains?(b, a)} do
+      {true, true} -> :equal
+      {true, false} -> :superlist
+      {false, true} -> :sublist
+      {false, false} -> :unequal
     end
   end
 
-  defp is_sublist?(list_a, list_b) when length(list_a) <= length(list_b) do
-    List.starts_with?(list_b, list_a) || is_sublist?(list_a, tl(list_b))
-  end
+  # determines if list a contains list b. restore_a is needed to restore
+  # already "eaten" members of a, when b couldn't be matched completely
+  defp contains?(a, b, current_b \\ :initial, restore_a \\ nil)
+  defp contains?(a, b, :initial, nil), do: contains?(a, b, b, nil)
+  defp contains?([ha | restore_a], b, b, nil), do: contains?()
 
-  defp is_sublist?(_, _), do: false
-end
+
+
+
+#   def compare(a, b) when is_list(a) and is_list(b) do
+#     cond do
+#       a == b -> :equal
+#       length(a) < length(b) -> if(is_sublist?(a, b), do: :sublist, else: :unequal)
+#       length(a) > length(b) -> if(is_sublist?(b, a), do: :superlist, else: :unequal)
+#       true -> :unequal
+#     end
+#   end
+
+#   defp is_sublist?(list_a, list_b) when length(list_a) <= length(list_b) do
+#     List.starts_with?(list_b, list_a) || is_sublist?(list_a, tl(list_b))
+#   end
+
+#   defp is_sublist?(_, _), do: false
+# end
---
+++ triangle/lib/triangle copy.ex
@@ -0,0 +1,21 @@
+defmodule Trianglee do
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  @doc """
+  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
+  """
+  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+  def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0,
+    do: {:error, "all side lengths must be positive"}
+
+  def kind(a, b, c) do
+    cond do
+      a + b >= c and b + c >= a and a + c >= b -> check_triangle_kind(a, b, c)
+      true -> {:error, "side lengths violate triangle inequality"}
+    end
+  end
+
+  defp check_triangle_kind(a, b, c) when a == b and b == c, do: {:ok, :equilateral}
+  defp check_triangle_kind(a, b, c) when a == b or b == c or a == c, do: {:ok, :isosceles}
+  defp check_triangle_kind(a, b, c) when a != b and b != c and a != c, do: {:ok, :scalene}
+end
